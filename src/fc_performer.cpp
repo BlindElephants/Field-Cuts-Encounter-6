8becd0e5506fdc41b7039f01bb736fe54cbd34ae
Jeremy Stewart <jeremy.ste@gmail.com>
2016-03-28 22:31:56 -0400

    Update fc_performer.cpp

--- src/fc_performer.cpp
+++ src/fc_performer.cpp
@@ -19,8 +19,8 @@ void fc_performer::setDeviceReferences(vector < fc_device* > _devices) {
     conditionManager -> setDeviceReference(devices);
 }
 void fc_performer::makeNewCondition(int _targetRelayChannel, int _sourceDevice, Parameter _x_y_z, Parameter _abs_del, Parameter _MT_LT, float _threshold, Lifespan _conditionLifespan, float _conditionTimer) {conditionManager -> makeNewCondition(_targetRelayChannel, _sourceDevice, _x_y_z, _abs_del, _MT_LT, _threshold, OPEN, 0, _conditionLifespan, _conditionTimer);}
-int fc_performer::getWristIndex() {return wristIndex;}
-int fc_performer::getPackIndex() {return packIndex;}
+int  fc_performer::getWristIndex() {return wristIndex;}
+int  fc_performer::getPackIndex() {return packIndex;}
 void fc_performer::drawConditions(float _x, float _y) {conditionManager -> drawConditionStreams(_x, _y, 100);}
 void fc_performer::drawDeviceDurRec(float _x, float _y) {devices[packIndex] -> drawSetDurRec(_x, _y);}
 void fc_performer::deleteAllConditions() {conditionManager -> deleteAllConditions();}
